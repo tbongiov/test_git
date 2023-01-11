b4d64e2765a3b113ca5a30dcfaab83a285bf47f5
tbongiov <tbongiov@sjrwmd.com>
2023-01-11 08:54:11 -0500

    add python simple_math program

---
+++ simple_math.py
@@ -0,0 +1,16 @@
+
+def addition(num1, num2):
+    # This adds 2 numbers
+    return num1+num2
+
+def multiplication(num1, num2):
+    # This multiplies 2 numbers
+    return num1*num2
+
+print('addition test')
+sum_of_two_numbers=addition(5,2)
+print(sum_of_two_numbers)
+
+print('multiplication test')
+mult_of_two_numbers=multiplication(5,2)
+print(mult_of_two_numbers)
